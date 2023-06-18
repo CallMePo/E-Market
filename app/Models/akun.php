4681b9206cbe01050c052f0ba4d5f2b974b39be6
mhaikalpa <muhammad.haikal1312@gmail.com>
2023-06-18 15:48:02 +0700

    15:47 MHaikal

--- app/Models/akun.php
+++ app/Models/akun.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\akun as Authenticatable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
 class akun extends Authenticatable
 {
@@ -56,5 +57,5 @@ class akun extends Authenticatable
     {
         return akun::where('remember_token', '=', $remember_token)->first();
     }
-    
+
 }
